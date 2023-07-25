34423cd49ff5058091b8307d2cfe1e550c499ab8
elesh75 <oguntademujaheed2@gmail.com>
2023-07-25 14:13:46 +0100

    2nd

---
+++ Cocktailss/app.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sleep 30
+
+sudo apt update -y
+
+sudo apt install -y gcc-c++ make
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt install -y nodejs
+
+sudo apt install unzip -y
+cd ~/ && unzip cocktails.zip
+cd ~/cocktails && npm i --only=prod   # this will make and cd into cocktails and install only prod dependencies
+
+sudo mv /tmp/cocktails.service /etc/systemd/system/cocktails.service
+sudo systemctl enable cocktails.service
+sudo systemctl start cocktails.service
